d0dbb02c4f5926f3c94add4c92478f5d786d670a
AbdallahQashmar <113013733+AbdallahQashmar@users.noreply.github.com>
2022-11-27 19:50:12 +0400

    Update Java.js
    I added java to the signup page to make it look better.

--- JS/Java.js
+++ JS/Java.js
@@ -1,1 +1,29 @@
-//Javascript
+  var helpText = document.getElementById("hekpText");
+
+  function init()
+  {
+  	helpText = document.getElementById("helpText");
+
+	document.getElementById("name").addEventListener("focus",
+	function() {helpText.innerHTML = "Enter your full name in this textbox.";},
+	false);
+
+	document.getElementById("email").addEventListener("focus",
+	function() {helpText.innerHTML = "Enter your email address in this textbox.";},
+	false);
+
+	document.getElementById("number").addEventListener("focus",
+	function() {helpText.innerHTML = "Enter your Phone Number in this textbox.";},
+	false);
+
+	document.getElementById("pass").addEventListener("focus",
+	function() {helpText.innerHTML = "Create a password in this textbox.";},
+	false);
+
+	document.getElementById("re-pass").addEventListener("focus",
+	function() {helpText.innerHTML = "re-Enter your password in this textbox.";},
+	false);
+
+  }
+
+  window.addEventListener("load", init, false);
