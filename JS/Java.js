578a7842e6c8df15fd39cd3e2233bdc6438a4574
Yasmeen Sabri <113013598+ysmeen24@users.noreply.github.com>
2022-11-28 10:30:40 +0400

    Update Java.js

--- JS/Java.js
+++ JS/Java.js
@@ -1,30 +1,83 @@
-
 var helpText = document.getElementById("helpText");
+	var myInput = document.getElementById("psw");
+	var letter = document.getElementById("letter");
+	var capital = document.getElementById("capital");
+	var number = document.getElementById("number");
+	var length = document.getElementById("length");
+	function init()
+  		{
+  			helpText = document.getElementById("helpText");
 
-  function init()
-  {
-  	helpText = document.getElementById("helpText");
+			document.getElementById("name").addEventListener("focus",
+			function() {helpText.innerHTML = "Enter your full name in this textbox.";},
+			false);
 
-	document.getElementById("name").addEventListener("focus",
-	function() {helpText.innerHTML = "Enter your full name in this textbox.";},
-	false);
+			document.getElementById("email").addEventListener("focus",
+			function() {helpText.innerHTML = "Enter your email address in this textbox.";},
+			false);
 
-	document.getElementById("email").addEventListener("focus",
-	function() {helpText.innerHTML = "Enter your email address in this textbox.";},
-	false);
+			document.getElementById("number").addEventListener("focus",
+			function() {helpText.innerHTML = "Enter your Phone Number in this textbox.";},
+			false);
 
-	document.getElementById("number").addEventListener("focus",
-	function() {helpText.innerHTML = "Enter your Phone Number in this textbox.";},
-	false);
+			document.getElementById("confirm_password").addEventListener("focus",
+			function() {helpText.innerHTML = "Re-Enter your password in this textbox.";},
+			false);
+  		}
 
-	document.getElementById("pass").addEventListener("focus",
-	function() {helpText.innerHTML = "Create a password in this textbox.";},
-	false);
+	window.addEventListener("load", init, false);
 
-	document.getElementById("re-pass").addEventListener("focus",
-	function() {helpText.innerHTML = "re-Enter your password in this textbox.";},
-	false);
+	myInput.onfocus = function()	{ document.getElementById("message").style.display = "block";}
 
-  }
 
-  window.addEventListener("load", init, false);
+	myInput.onblur = function() { document.getElementById("message").style.display = "none"; }
+
+
+	myInput.onkeyup = function() {
+  		var lowerCaseLetters = /[a-z]/g;
+  		if(myInput.value.match(lowerCaseLetters)) {  
+    			letter.classList.remove("invalid");
+    			letter.classList.add("valid");}
+
+		 else {
+			letter.classList.remove("valid");
+    			letter.classList.add("invalid");}
+  
+
+  		var upperCaseLetters = /[A-Z]/g;
+  		if(myInput.value.match(upperCaseLetters)) {  
+    			capital.classList.remove("invalid");
+    			capital.classList.add("valid");}
+
+		else {
+    			capital.classList.remove("valid");
+    			capital.classList.add("invalid");}
+
+ 		var numbers = /[0-9]/g;
+  		if(myInput.value.match(numbers)) {  
+    			number.classList.remove("invalid");
+    			number.classList.add("valid"); } 
+
+		else {
+			number.classList.remove("valid");
+    			number.classList.add("invalid");}
+  
+  		if(myInput.value.length >= 8) {
+    			length.classList.remove("invalid");
+    			length.classList.add("valid");}
+
+		else {
+    			length.classList.remove("valid");
+    			length.classList.add("invalid"); }
+	}
+	var password = document.getElementById("psw"), confirm_password = document.getElementById("confirm_password");
+
+	function validatePassword(){
+		if(password.value != confirm_password.value) {
+    			confirm_password.setCustomValidity("Passwords Don't Match");}
+		 else {
+    			confirm_password.setCustomValidity('');}
+	}
+	
+	password.onchange = validatePassword;
+	confirm_password.onkeyup = validatePassword;
